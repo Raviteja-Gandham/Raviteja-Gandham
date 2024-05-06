792bf4c044560a43612458a629d180e27dd59a39
Raviteja-Gandham <168612928+Raviteja-Gandham@users.noreply.github.com>
2024-05-06 22:21:31 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Ascii
+{
+	public static void main(String a[])
+	{
+		char c;
+		Scanner sr=new Scanner(System.in);
+		c =sr.next().charAt(0); 
+		
+		int ascii_value;
+		ascii_value=(int)c;
+		
+		System.out.println(ascii_value);
+	}
+}
