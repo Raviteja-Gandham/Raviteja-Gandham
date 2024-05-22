e3e651877ed130b2ec27ac7b6e8a88d4ab92268a
Raviteja-Gandham <168612928+Raviteja-Gandham@users.noreply.github.com>
2024-05-22 20:43:05 +0530

    EncapsulSetterGetterParaConsdtructor1.java

--- EncapsulSetterGetterParaConsdtructor1.java
+++ EncapsulSetterGetterParaConsdtructor1.java
@@ -1,9 +1,9 @@
 
 public class EncapsulSetterGetterParaConsdtructor1 {
 		
-	private int id;
-	private String name;
-	private int salary;
+	private static int id;
+	private static String name;
+	private static int salary;
 
 	public EncapsulSetterGetterParaConsdtructor1(int id, int salary, String name) {
 		super();
@@ -15,7 +15,9 @@ public class EncapsulSetterGetterParaConsdtructor1 {
 	public static void main(String[] args) {
 
 		EncapsulSetterGetterParaConsdtructor1 ep=new EncapsulSetterGetterParaConsdtructor1(81,85000,"Raviteja");
-		
+		System.out.println("Id  : "+id);
+		System.out.println("Name  : "+name);
+		System.out.println("Salary  : "+salary);
 	}
 
 }
